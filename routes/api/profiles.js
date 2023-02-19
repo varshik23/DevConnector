59001043c86a436f8e212e6be8f142c22619159c
varshik sonti <sontivarshik@gmail.com>
2023-02-18 18:04:26 -0700

    Updated the route for profiles

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
 const router = express.Router();
 
-// path api/profiles
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// path api/profiles/me
 // auth private
-router.get('/', (req, res) => {
-    res.send("Welcome to profiles page");
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+        if(!profile) {
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 module.exports = router
